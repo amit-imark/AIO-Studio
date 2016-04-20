7aea3792c904f5d8706e8dd50480b05f1a9ad17f
amit-imark <amit.nanda@imarkinfotech.com>
2016-04-20 21:41:19 +0530

    resolve testing bugs

--- js/my-script.js
+++ js/my-script.js
@@ -6,6 +6,24 @@ jQuery(document).ready(function(){
 	jQuery('.banner-section').addClass('animat'); 
 	 
 	 });	
+	 
+	 var stickyNavTop = $('.btn-trigger').offset().top;
+ 
+var stickyNav = function(){
+var scrollTop = $(window).scrollTop();
+      
+if (scrollTop > stickyNavTop) { 
+    jQuery('.btn-trigger').addClass('sticky');
+} else {
+    jQuery('.btn-trigger').removeClass('sticky'); 
+}
+};
+ 
+stickyNav();
+ 
+$(window).scroll(function() {
+    stickyNav();
+});
  
  });
 	  
